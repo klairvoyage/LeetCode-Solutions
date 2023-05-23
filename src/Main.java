c1673a4c60eb90117341a1f281f91cc4d86a9818
Abraham <abrahamwondimu03@gmail.com>
2023-05-23 11:48:53 -0500

    added some Tests

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Nr9_PalindromeNumber test1 = new Nr9_PalindromeNumber();
+        /*System.out.println(test1.isPalindrome(9237329));
+        System.out.println(test1.isPalindrome(923329));
+        System.out.println(test1.isPalindrome(10));
+        System.out.println(test1.isPalindrome(9));
+        System.out.println(test1.isPalindrome(1));
+        System.out.println(test1.isPalindrome(0));
+        System.out.println(test1.isPalindrome(-0));
+        System.out.println(test1.isPalindrome(-1));
+        System.out.println(test1.isPalindrome(-9));
+        System.out.println(test1.isPalindrome(-10));
+        System.out.println(test1.isPalindrome(-4224))
+        System.out.println(test1.isPalindrome(-42024));*/
+
+        Nr125_ValidPalindrome test2 = new Nr125_ValidPalindrome();
+        /*System.out.println(test2.isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println(test2.isPalindrome("race a car"));
+        System.out.println(test2.isPalindrome(" "));
+        System.out.println(test2.isPalindrome_alt("A man, a plan, a canal: Panama"));
+        System.out.println(test2.isPalindrome_alt("race a car"));
+        System.out.println(test2.isPalindrome_alt(" "));*/
+
+        //Nr13_RomanToInteger test3 = new Nr13_RomanToInteger();
+        /*System.out.println(test3.romanToInt("XXVVII"));*/
     }
 }
